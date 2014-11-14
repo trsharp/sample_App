3cdf063f1bfc0a9691d015e7bff363ce36916b8c
trsharp <trsharp@Csustudent.net>
2014-11-13 20:58:20 -0500

    asf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '3460601f85eb29a6b6dc5c6c62d65fa4378c7909767829992d0d23325c6830112659e18250b461d64de6fd1903844f4169aee66dcad897013c23c3a51c51d8d6'
---
+++ test/helpers/statc_pages_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class StatcPagesHelperTest < ActionView::TestCase
+end
